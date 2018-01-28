e985eb831506dde00d27ee1cf2badd860f9a02c8
automaticdai <automatic.dai@gmail.com>
2018-01-28 20:10:10 +0000

    Improved intents and formats.

--- Line.m
+++ Line.m
@@ -1,82 +1,84 @@
 function [Edge] = Line(lambda,psi,Edge)%lambda(L,BS) , psi(L,R,Ty)
+
 global ploti
 set(0,'DefaultTextInterpreter','Latex');
-c9=Edge;
-CB=0;
+c9 = Edge;
+CB = 0;
 %% Edge Remover by Psi Note: Psi here considered Nx4 not Nx5 maybe in future quadrangular would add that require 1 more
-if psi == 0  
+if psi == 0
 else
-for i=1:1:numel(psi(:,1)) %Counter for edges 
-if psi(i,4)==1
-    j=1;
-while j<=(numel(c9)/2) %Counter for edges 
-      if (((((psi(i,1)-c9(j,1))^2) + ((psi(i,2)-c9(j,2))^2))^(0.5)) <= psi(i,3))
-%The condition to check the boundery property
-c9(j,:)=[];
-j=j-1;
-      end
-j=j+1;
-end
-%elseif psi(:,4)==2%WILL BE FILLED for SQUARE
-%WILL BE FILLED
+for i = 1:1:numel(psi(:,1)) %Counter for edges
+if psi(i,4) == 1
+  j = 1;
+  while j <= (numel(c9)/2) %Counter for edges
+    if (((((psi(i,1)-c9(j,1))^2) + ((psi(i,2)-c9(j,2))^2))^(0.5)) <= psi(i,3))
+      %The condition to check the boundery property
+      c9(j,:) = [];
+      j = j-1;
+    end
+    j = j+1;
+  end
+  %elseif psi(:,4)==2%WILL BE FILLED for SQUARE
+  %WILL BE FILLED
 end
 th = 0:pi/50:2*pi;%for loop for creating circle
 xunit = (psi(i,3)+CB) * cos(th) + psi(i,2);%equation of circle :D
 yunit = (psi(i,3)+CB) * sin(th) + psi(i,1);
 hold on
 subplot(1,2,1)
-ploti=plot(xunit, yunit,'y');%Plot the boys :v 
+ploti = plot(xunit, yunit,'y');%Plot the boys :v
     xlim([1 640])
     ylim([1 480])
 end
 end
 hold on
 subplot(1,2,1)
-ploti=plot(c9(:,2),c9(:,1),'y.'); %edges
+ploti = plot(c9(:,2),c9(:,1),'y.'); %edges
 %
-%% Edge grouper by BS of lambda 
-DV=0;%Data Variable for categorizing the edges in tree form collections
-f=1; %the counter of new column edge groups
-k=0; %shifter triger for new column edge groups
-i=1;% simple counter 
+%% Edge grouper by BS of lambda
+DV = 0;%Data Variable for categorizing the edges in tree form collections
+f = 1; %the counter of new column edge groups
+k = 0; %shifter triger for new column edge groups
+i = 1;% simple counter
 %-------in Lambda
-    for z=1:1:(numel(lambda(:,1))) %Lambda Counter
+    for z = 1:1:(numel(lambda(:,1))) %Lambda Counter
 %------------- %Matrix Shifter
-if k==1 
-f=f+2;%shift two for x and y 
-k=0;
+if k == 1
+  f = f + 2;%shift two for x and y
+  k = 0;
 end
 hold on
 %--------------------- The Lambda edge comparator
-j=1;
-g=1;
-if lambda==0
+j = 1;
+g = 1;
+if lambda == 0
 else
-while j<=(numel(c9)/2)
-      if (((((lambda(z,1)-c9(j,1))^2) + ((lambda(z,2)-c9(j,2))^2))^(0.5)) <= lambda(z,3)) %The condition to check the boundery property
-subplot(1,2,1)
-plot([lambda(z,2), c9(j,2)],[lambda(z,1), c9(j,1)], 'g'); %it plots the related lines when condition satisfied
-DV(g,f)=c9(j,1);%include the edges to the relevant group
-DV(g,f+1)=c9(j,2);
-c9(j,:)=[];
-k=1;%Activation for new column couple
-g=g+1;
-      else
-j=j+1;
-      end 
-end
+  while j <= (numel(c9)/2)
+    if (((((lambda(z,1)-c9(j,1))^2) + ((lambda(z,2)-c9(j,2))^2))^(0.5)) <= lambda(z,3)) %The condition to check the boundery property
+      subplot(1,2,1)
+      plot([lambda(z,2), c9(j,2)],[lambda(z,1), c9(j,1)], 'g'); %it plots the related lines when condition satisfied
+      DV(g,f) = c9(j,1);%include the edges to the relevant group
+      DV(g,f+1) = c9(j,2);
+      c9(j,:) = [];
+      k = 1;%Activation for new column couple
+      g = g+1;
+    else
+      j = j+1;
+    end
+  end
 end
 %--------------------------------------
     end % foraaa\
-%-----------------Boxing part :D 
+%-----------------Boxing part :D
 %%
 %Out of Lambda Members
-for j=1:1:(numel(c9)/2) %Counter for edges 
-f=f+2;
+for j = 1:1:(numel(c9)/2) %Counter for edges
+f = f+2;
 %plot(c9(j,2),c9(j,1),'r'); %it plots the related lines when condition satisfied
-DV(j,f)=c9(j,1);%include the edges to the relevant group
-DV(j,f+1)=c9(j,2);
+DV(j,f) = c9(j,1);%include the edges to the relevant group
+DV(j,f+1) = c9(j,2);
 end
 hold on
-Edge=DV;
-end
+Edge = DV;
+
+end % end of function
--- TrailMainSourceImageSERIES.m
+++ TrailMainSourceImageSERIES.m
@@ -5,33 +5,37 @@ set(0,'DefaultTextInterpreter','Latex');
 %preview(cam)
 %Main intial conditions
 %--------------------------- Algorithm Constants
-lambda=0;
-psi=0;%first is x next is y? O_
-En=0;
-Er=0;
-C=0;
-Cr=0;
-delta=[0 0 0 0;0 0 0 0;0 0 0 0;0 0 0 0;0 0 0 0];
+lambda = 0;
+psi = 0;%first is x next is y? O_
+En = 0;
+Er = 0;
+C = 0;
+Cr = 0;
+delta = [0 0 0 0;
+         0 0 0 0;
+         0 0 0 0;
+         0 0 0 0;
+         0 0 0 0];
 %----------------------------
-alpha=[0 0 0 0 0 0 0];
+alpha = [0 0 0 0 0 0 0];
 global deltay deltaz Trs Trcr Trmax Av
-global frame Vv 
-Trs=3;
-Trcr=2;
-Trmax=5;
-Dv=.05;
-Av=.001;
-Vv=.05;
-deltay=8;
-deltaz=8;
+global frame Vv
+Trs = 3;
+Trcr = 2;
+Trmax = 5;
+Dv = 0.05;
+Av = 0.001;
+Vv = 0.05;
+deltay = 8;
+deltaz = 8;
 %----image series
-drs='./Approaching_Boxi'; % in current directory
-dr1=dir([drs '/*.png']); % get only jpg
-f1={dr1.name};% get only filenames to cell
+drs = './Approaching_Boxi'; % in current directory
+dr1 = dir([drs '/*.png']); % get only jpg
+f1 = {dr1.name};% get only filenames to cell
 
-for c=1:length(f1) % for each image
-    tic
-i=imread([drs '/' f1{c}]);
+for c = 1:length(f1) % for each image
+  tic
+i = imread([drs '/' f1{c}]);
 %image series
 %BL=100; %boundery layer Cylindrical-tin
 %img = snapshot(cam);%begin snap
@@ -43,76 +47,75 @@ figure(c)
 %subplot(length(f1)/4,length(f1)/5,c)
 %Make image greyscale
 if length(size(i)) == 3
-	im =  double(i(:,:,2));
+  im =  double(i(:,:,2));
 else
-	im = double(i);
+  im = double(i);
 end
 
-c9 = fast9(im, 25,1);
+c9 = fast9(im, 25, 1);
 axis image
 colormap(gray)
 subplot(1,2,1)
-hold on 
+hold on
 imshow(im / max(im(:)));
 subplot(1,2,2)
-hold on 
+hold on
 imshow(im / max(im(:)));
 hold on
 subplot(1,2,1)
 plot(c9(:,1),c9(:,2),'r.'); %edges
 hold  on
-c9=[c9(:,2),c9(:,1)];
-if c==1
-Size(c,1)=numel(c9(:,1));    
+c9 = [c9(:,2),c9(:,1)];
+if c == 1
+  Size(c,1) = numel(c9(:,1));
 else
-Size(c,1)=numel(c9(:,1))+Size(c-1,1);
+  Size(c,1) = numel(c9(:,1))+Size(c-1,1);
 end
-Size(c,6)=numel(c9(:,1));
-Edge=c9;
+Size(c,6) = numel(c9(:,1));
+Edge = c9;
 %--------Algo begins HERE ......!!!!!
 Edge = Line(lambda,psi,Edge);
-    frame=1; %Every Sec one frame! Works
+frame = 1; %Every Sec one frame! Works
 [En,Er,C,Cr,psi,lambda,alpha,delta] = Circle(Edge,C,Cr,En,Er,psi,delta,Vv,Dv,lambda,alpha);
-Size(c,2)=numel(En(:,1));
-Size(c,3)=numel(Er(:,1));
-Size(c,4)=numel(C(:,1));
-Size(c,5)=numel(Cr(:,1));
+Size(c,2) = numel(En(:,1));
+Size(c,3) = numel(Er(:,1));
+Size(c,4) = numel(C(:,1));
+Size(c,5) = numel(Cr(:,1));
 %delta
 %delta=[0 0 0 0;0 0 0 0;0 0 0 0;0 0 0 0;0 0 0 0];
 hold on
 subplot(1,2,1)
-ploti=plot(En(:,2),En(:,1),'bs');
-    xlim([1 640])
-    ylim([1 480])
-hold on 
+ploti = plot(En(:,2),En(:,1),'bs');
+xlim([1 640])
+ylim([1 480])
+hold on
 th = 0:pi/50:2*pi;%for loop for creating circle
 CB=1;
 hold on
-    if C==0
-    else
+  if C == 0
+  else
     for i=1:1:(numel(C(:,1)))
-    xunit = (C(i,3)+CB) * cos(th) + C(i,2);%equation of circle :D
-    yunit = (C(i,3)+CB) * sin(th) + C(i,1);
-    subplot(1,2,2)
-    ploti=plot(xunit, yunit,'g');%Plot the boys :v 
-    xlim([1 640])
-    ylim([1 480])
-    end 
+      xunit = (C(i,3)+CB) * cos(th) + C(i,2);%equation of circle :D
+      yunit = (C(i,3)+CB) * sin(th) + C(i,1);
+      subplot(1,2,2)
+      ploti = plot(xunit, yunit,'g');%Plot the boys :v
+      xlim([1 640])
+      ylim([1 480])
     end
-    CB=1;
-    if Cr==0
-    else
+  end
+  CB = 1;
+  if Cr == 0
+  else
     for i=1:1:(numel(Cr(:,1)))
-    xunit = (Cr(i,3)+CB) * cos(th) + Cr(i,2);%equation of circle :D
-    yunit = (Cr(i,3)+CB) * sin(th) + Cr(i,1);
-    subplot(1,2,2)
-    ploti=plot(xunit, yunit,'r');%Plot the boys :v 
-    xlim([1 640])
-    ylim([1 480])
- 
+      xunit = (Cr(i,3)+CB) * cos(th) + Cr(i,2);%equation of circle :D
+      yunit = (Cr(i,3)+CB) * sin(th) + Cr(i,1);
+      subplot(1,2,2)
+      ploti = plot(xunit, yunit,'r');%Plot the boys :v
+      xlim([1 640])
+      ylim([1 480])
     end
-    end
-    hold on 
+  end
+  hold on
 subplot(1,2,2)
 txt = ['Frame ',num2str(c)];
 title(txt,'FontSize',16)
@@ -124,11 +127,11 @@ txt = ['Frame ',num2str(c)];
 title(txt,'FontSize',16)
 hold on
 xlabel('$E_n$, $Edge$, $\lambda$ and $\psi$','FontSize',16)
-set(gcf,'Units','Inches');  
+set(gcf,'Units','Inches');
 pos = get(gcf,'Position');
 set(gcf,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',[pos(3), pos(4)])
 %----------Plot
- temp=['fig',num2str(c),'.fig'];
+ temp = ['fig',num2str(c),'.fig'];
  saveas(gca,temp);
 % %--------------------
 end
