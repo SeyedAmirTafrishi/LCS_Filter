aacf2bbac79476a150ee0cd3b1a97237eed62863
automaticdai <automatic.dai@gmail.com>
2018-05-16 10:37:32 +0100

    fixed angle calculation

--- helpers/calculate_vector_angle.m
+++ helpers/calculate_vector_angle.m
@@ -20,8 +20,8 @@ function [ angle ] = calculate_vector_angle( x1, y1, x2, y2 )
         angle=180;
     end
 
-    angle2 = atan2(x2 - x1, y2 - y1) * (180/pi);
+    angle2 = atan2(y1 - y2, x1 - x2) * (180/pi);
 
-    %angle
-    %angle2
+    angle
+    angle2
 end
