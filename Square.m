ba9e8a6adeec7eca3a4294af2822812b21ef2ca1
automaticdai <automatic.dai@gmail.com>
2018-12-08 21:05:29 +0000

    add skelton of function Square

--- Square.m
+++ Square.m
@@ -1,5 +1,29 @@
 function [S, C, Cr] = Square(S, C, Cr, delta, Vv, Dv)
 
+% Take a normal circle
 
+
+% Find a furthest distance of circle at same velocity
+
+
+% Does couple C(A) and C(B) found?
+
+
+% Any normal/rebel circle found?
+
+
+% Include any circle that matches same velocity and certain angle
+
+
+% Create square and estimated with any matched S'(k-1)
+
+
+% Does just unmatched circles left (no match of case A and B)?
+
+
+% Match remaining circles as a squre
+
+
+% return S
 
 end
