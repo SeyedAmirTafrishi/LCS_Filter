c4934baeb146fab4315ca0b7c73523cf482cd399
SeyedAmirTafrishi <amirtafrishi@yahoo.com>
2018-12-09 21:15:01 +0900

    Update Square.m

--- Square.m
+++ Square.m
@@ -1,9 +1,38 @@
 function [S, C, Cr] = Square(S, C, Cr, delta, Vv, Dv)
-
+% Subsitute C and Cr to new Ct (Temprery matrix) 
 % Take a normal circle
-while u <= (numel(C(:,1)))
+while u <= (numel(C(:,1))) 
     % Find a furthest distance of circle at same velocity
+% D_D = 0 distance of further (Maybe maximum value initiation)
+% Same while loop for Ct (1) 
+% if condition (furthest distance) && (Angle and velocity) Match CASE A
+% WE have couple C(A) and C(B)
+% Apply Unified Circle to the two circles
+% State: Search loop for verifying any circle exsitis that makes two C(A) and C(B)
+% as seperate circle 
+% IF condition (2) V*_C < V_C(B), V_C(A)
+% Same while loop excluding C(A) and C(B) 
+% Find matches of exstiing circles
+% if matches are over
+% State Remove the circles from Ct
+% Square While loop
+% Find Square matching the create square from C(A) and C(B) couples
+% Estimate the Square 
+% end all loops 
+% Else of If CASE A (No match) (1) CASE B
+% Find C(A) where it is align with other circles in delta Beta
+% Loop for C_t 
+% Construct the unified circle
+% Loop for Square search
+% Estimate for Square
+% Remove the Circles From C_t
+%ELSE of IF (2) 
+%decrease the D_D
+% Return to while loop of 1 to fin
 
+% end
+
+% 
     % Does couple C(A) and C(B) found?
 
     % Any normal/rebel circle found?
@@ -17,9 +46,10 @@ while u <= (numel(C(:,1)))
 end
 
 
+
 % Match remaining circles as a squre
 % S = []
 
-% return S
+% return S delta Psi 
 
 end
