6378c11aa2c107a061256c5b2855d1243ce05ba6
automaticdai <automatic.dai@gmail.com>
2019-07-07 10:48:07 +0100

    improved vector angle calculation; added a new class: Vector

--- helpers/relative_angle_of_two_vectors.m
+++ helpers/relative_angle_of_two_vectors.m
@@ -1,8 +1,8 @@
-function [ angle_in_degree ] = relative_angle_of_two_vectors(vec1_x, vec1_y, vec2_x, vec2_y, ICX, ICY)
+function [ angle_in_degree ] = relative_angle_of_two_vectors(vec1_x, vec1_y, vec2_x, vec2_y, vec1_icx, vec1_icy, vec2_icx, vec2_icy)
 %func angle_of_two_vectors()
 %   Angle is calculated in screen coordination. Return value is in degrees.
-    ang1 = calculate_vector_angle(vec1_x, vec1_y, ICX, ICY);
-    ang2 = calculate_vector_angle(vec2_x, vec2_y, ICX, ICY);
+    ang1 = calculate_vector_angle(vec1_x, vec1_y, vec1_icx, vec1_icy);
+    ang2 = calculate_vector_angle(vec2_x, vec2_y, vec2_icx, vec2_icy);
     ang_diff = ang1 - ang2;
     angle_in_degree = abs(ang_diff);
     
