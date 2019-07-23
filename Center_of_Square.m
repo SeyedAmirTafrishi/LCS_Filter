375ae9418552960dd124e5874bd8c8142591af59
SeyedAmirTafrishi <amirtafrishi@yahoo.com>
2019-07-24 00:22:56 +0900

    Center of Square solved
    I solve the centring problem ;)

--- Center_of_Square.m
+++ Center_of_Square.m
@@ -1,6 +1,9 @@
-CanswerA=[1 2 1;3 10 4;19 3 2];
-for i=1:1:numel(CanswerA(:,1))
+%Problem Solved :D center of Square
 
+CanswerA=[1 2 5;3 10 4;12 3 5];
+for i=1:1:numel(CanswerA(:,1))
+xunit=0;
+yunit=0;
     th = 0:pi/50:2*pi;%for loop for creating circle
     CB = 1;
             xunit = (CanswerA(i,3) ) * cos(th) + CanswerA(i,2);%equation of circle :D
@@ -15,18 +18,18 @@ plot(Cv_X,Cv_Y,'- xb','MarkerSize', 18,'LineWidth' , 2.5)
 % CanswerAN=CanswerA(:,1)-Cv_Y
 % CanswerAN=CanswerA(:,2)-Cv_X
 
-TempYPositive=max(CanswerA(:,1)-Cv_Y+CanswerA(:,3)) %Y 
-TempYNegaitive=min(CanswerA(:,1)-Cv_Y-CanswerA(:,3)) %Y 
-TempXPositive=max(CanswerA(:,2)-Cv_X+CanswerA(:,3)) %Y 
-TempXNegaitive=min(CanswerA(:,2)-Cv_X-CanswerA(:,3)) %Y    
+TempYPositive=max(CanswerA(:,1)+CanswerA(:,3)) %Y It find the max of array!!!!
+TempYNegaitive=min(CanswerA(:,1)-CanswerA(:,3)) %Y 
+TempXPositive=max(CanswerA(:,2)+CanswerA(:,3)) %Y 
+TempXNegaitive=min(CanswerA(:,2)-CanswerA(:,3)) %Y    
 
-Y_o=((TempYPositive-TempYNegaitive)/2)+Cv_Y
-X_o=((TempXPositive-TempXNegaitive)/2)+Cv_X
-a=abs(TempYPositive-TempYNegaitive)/2+Cv_Y; %height
-b=abs(TempXPositive-TempXNegaitive)/2+Cv_X;
+Y_o=((TempYPositive-TempYNegaitive)/2)
+X_o=((TempXPositive-TempXNegaitive)/2)
+a=abs(TempYPositive-TempYNegaitive)/2 %height
+b=abs(TempXPositive-TempXNegaitive)/2
 plot(X_o,Y_o,'- *b','MarkerSize', 18,'LineWidth' , 2.5)  
 
-plot([X_o-b X_o-b],[Y_o-a Y_o+a],'r')
-plot([X_o-b X_o+b],[Y_o-a Y_o-a],'r')
-plot([X_o+b X_o-b],[Y_o+a Y_o+a],'r')
-plot([X_o+b X_o+b],[Y_o+a Y_o-a],'r')
+plot([TempXPositive TempXPositive],[TempYNegaitive TempYPositive],'r')
+plot([TempXNegaitive TempXPositive],[TempYPositive TempYPositive],'r')
+plot([TempXNegaitive TempXNegaitive],[TempYNegaitive TempYPositive],'r')
+plot([TempXNegaitive TempXPositive],[TempYNegaitive TempYNegaitive],'r')
